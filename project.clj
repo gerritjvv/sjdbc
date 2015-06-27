fe23ec04ac6dbeeaca69531197bb3566bd2d5da4
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2015-06-27 20:09:21 +0200

    up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sjdbc "0.1.0"
+(defproject sjdbc "0.1.1"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
             :license {:name "Eclipse Public License"
